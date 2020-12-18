6e64f73e4add4c3cd6fa068d2c92bad408a6b46f
enginepdf <yeindap@gmail.com>
2020-12-19 01:44:06 +0900

    Update flatten.js
    Added some spaces and solved some errors

--- flatten.js
+++ flatten.js
@@ -3,9 +3,9 @@ function flatten(nestedArray) {
   function flt(arr){
     let temparr=[]
     for(let i = 0 ; i < arr.length ; i++){
-    if(!Array.isArray(arr[i])){
-     temparr.push(arr[i])
-    }  
+      if(!Array.isArray(arr[i])){
+        temparr.push(arr[i])
+      }  
     else if (Array.isArray(arr[i])){
      for(let j=0 ; j<arr[i].length ; j++){
        temparr.push(arr[i][j])
@@ -15,20 +15,22 @@ function flatten(nestedArray) {
 return temparr  
 }
 function recur(nestedArr){
- let count=0;
- let result=[]
-for (let i = 0 ; i < nestedArr.length ; i++){
-if(Array.isArray(nestedArr[i])){
-  result=[...result, ...flt(nestedArr[i])]
-  count=count+1
-} else
-result.push(nestedArr[i])
-}
-if(count===0){
-return result
-} else
-return recur(result)
-}
+   let count=0;
+   let result=[]
+  for (let i = 0 ; i < nestedArr.length ; i++){
+    if(Array.isArray(nestedArr[i])){
+      result=[...result, ...flt(nestedArr[i])]
+      count=count+1
+    } else{
+      result.push(nestedArr[i])
+      } 
+  }
+  if(count===0){
+    return result
+  } else{
+      return recur(result)
+    }
+  }
   
   let res=recur(nestedArray) 
   return res
